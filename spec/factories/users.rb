5e3d5d5105992d9a2563cf916980bfc9a1450b9b
AlexMasonAM <alexmmason@gmail.com>
2015-02-02 11:45:56 -0800

    added FactoryGirl gem used on intial Rspec User tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,12 @@ FactoryGirl.define do
   factory :user do
     username "MyString"
     email "MyString"
+    settings_private false
   end
 
+  factory :private_user, class: User do
+    username "MyString"
+    email  "MyString"
+    settings_private true
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "should validate presence of username" do
+    user = FactoryGirl.build(:user)
+    user.username = nil
+    expect(user).to be_invalid
+  end
+  it "should confirm that a user with a private settings is private" do
+    user = FactoryGirl.build(:private_user)
+    expect(user.settings_private).to eq(true)
+  end  
 end
